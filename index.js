e9b69209a4c437d8462bb5a1e864283f4b7033b1
“Vilde <vilde@vevatne.no>
2017-09-06 11:42:51 -0700

    Add nodemon as a dependency

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // use commonjs modules in the backend
 const express = require('express'),
 passport = require('passport'),
-GoogleStrategy = require('passport-google-oauth20').Strategy;
+GoogleStrategy = require('passport-google-oauth20').Strategy,
+keys = require('./config/keys');
+
 
 const app = express();
 
 // creates a new instance of the passport Google Strategy
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+		}, 
+		(accessToken, refreshToken, profile, done) => {
+		console.log('access token', accessToken);
+		}
+	)
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // dynamic port binding for Heroku deployment
 const PORT = process.env.PORT || 5000;
