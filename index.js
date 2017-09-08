83ea48bb5596735fdb5b8f4dfe3291fb45dd043c
“Vilde <vilde@vevatne.no>
2017-09-08 11:12:51 -0700

    Add cookies and make passport use it

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // use commonjs modules in the backend
 const express = require('express'),
-keys = require('./config/keys');
+keys = require('./config/keys'),
+cookieSession = require('cookie-session'),
+passport = require('passport'),
 mongoose = require('mongoose');
 require('./models/user');
 require('./services/passport');
@@ -8,6 +10,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // require function, call it with app as an argument
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,18 @@ mongoose = require('mongoose'),
 User = mongoose.model('users'),
 keys = require('../config/keys');
 
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+		.then(user => {
+			done(null, user);
+		})
+});
+
 // creates a new instance of the passport Google Strategy
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
@@ -11,7 +23,17 @@ passport.use(new GoogleStrategy({
 	callbackURL: '/auth/google/callback'
 		}, 
 		(accessToken, refreshToken, profile, done) => {
-			new User({ googleId: profile.id }).save();
+			User.findOne({ googleId: profile.id })
+				.then((existingUser) => {
+					if (existingUser) {
+						// first argument = error, second = object
+						done(null, existingUser);
+					} else {
+						new User({ googleId: profile.id })
+							.save()
+							.then(user => done(null, user));
+					}
+				})
 		}
 	)
 );
